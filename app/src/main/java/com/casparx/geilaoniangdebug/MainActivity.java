624581738fb2cfc813fca9d8b22848c787c59fc8
CasparGX <630248976@qq.com>
2016-04-24 09:39:49 +0800

    Update

--- app/src/main/java/com/casparx/geilaoniangdebug/MainActivity.java
+++ app/src/main/java/com/casparx/geilaoniangdebug/MainActivity.java
@@ -1,8 +1,17 @@
 package com.casparx.geilaoniangdebug;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.rey.material.widget.Button;
@@ -33,6 +42,8 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
     TextView tvTime;
     @Bind(R.id.tv_date)
     TextView tvDate;
+    @Bind(R.id.img_photo)
+    ImageView imgPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,18 +81,24 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         tpd.show(getFragmentManager(), "Timepickerdialog");
     }
 
+    @OnClick(R.id.btn_confirm)
+    void onClickBtnConfirm() {
+        Bitmap bitmap = drawTextToBitmap(this, R.drawable.test_img, "1234567890");
+        imgPhoto.setImageBitmap(bitmap);
+    }
+
     @Override
     public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
         view.getFirstDayOfWeek();
         String date = null;
-        date=year+"-"+monthOfYear+"-"+dayOfMonth+" "+getWeek(year, monthOfYear, dayOfMonth);
+        date = year + "-" + monthOfYear + "-" + dayOfMonth + " " + getWeek(year, monthOfYear, dayOfMonth);
         tvDate.setText(date);
     }
 
     @Override
     public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int second) {
         String time = null;
-        time = hourOfDay+":"+minute;
+        time = hourOfDay + ":" + minute;
         tvTime.setText(time);
     }
 
@@ -127,6 +144,51 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         }
         Log.i("getWeek", c.get(Calendar.DAY_OF_WEEK) + " " + Week);
         return Week;
+    }
+
+    /**
+     * 添加文字到图片，类似水印文字。
+     *
+     * @param gContext
+     * @param gResId
+     * @param gText
+     * @return
+     */
+    public static Bitmap drawTextToBitmap(Context gContext, int gResId, String gText) {
+        Resources resources = gContext.getResources();
+        float scale = resources.getDisplayMetrics().density;
+        Bitmap bitmap = BitmapFactory.decodeResource(resources, gResId);
+
+        Bitmap.Config bitmapConfig = bitmap.getConfig();
+        // set default bitmap config if none
+        if (bitmapConfig == null) {
+            bitmapConfig = Bitmap.Config.ARGB_8888;
+        }
+        // resource bitmaps are imutable,
+        // so we need to convert it to mutable one
+        bitmap = bitmap.copy(bitmapConfig, true);
+
+        Canvas canvas = new Canvas(bitmap);
+        // new antialised Paint
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        // text color - #3D3D3D
+        paint.setColor(Color.rgb(61, 61, 61));
+        // text size in pixels
+        paint.setTextSize((int) (14 * scale * 5));
+        // text shadow
+        paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
+
+        // draw text to the Canvas center
+        Rect bounds = new Rect();
+        paint.getTextBounds(gText, 0, gText.length(), bounds);
+        //      int x = (bitmap.getWidth() - bounds.width()) / 2;
+        //      int y = (bitmap.getHeight() + bounds.height()) / 2;
+        //draw  text  to the bottom
+        int x = (bitmap.getWidth() - bounds.width()) / 10 * 9;
+        int y = (bitmap.getHeight() + bounds.height()) / 10 * 9;
+        canvas.drawText(gText, x, y, paint);
+
+        return bitmap;
     }
 
 }
